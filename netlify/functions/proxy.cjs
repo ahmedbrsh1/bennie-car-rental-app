7707ed1270a8857d21115b569ddc17a06ec9806b
ahmed brsh <ahmedbrsh@hotmail.com>
2025-04-02 03:42:27 +0200

    proper import

--- netlify/functions/proxy.cjs
+++ netlify/functions/proxy.cjs
@@ -1,4 +1,4 @@
-const fetch = import("node-fetch");
+import fetch from "node-fetch";
 
 exports.handler = async function (event, context) {
   const BACKEND_URL = process.env.BACKEND_URL || "http://localhost:8000";
